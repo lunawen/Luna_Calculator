4880cc21f4f7ba457fcd7c3d2f12e3e48f1116c9
Luna Wen <lunawen33@gmail.com>
2020-08-23 17:56:14 +1000

    Add project files.

---
+++ Luna_Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Luna_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
